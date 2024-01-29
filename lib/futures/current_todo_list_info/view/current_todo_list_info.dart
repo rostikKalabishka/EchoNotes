08e7802cc2ef7d3b96fc457a88a7174a4f806c11
RostikKalabishka <rostik31052002@gmail.com>
2024-01-29 18:57:36 +0200

    fix lottie

--- lib/futures/current_todo_list_info/view/current_todo_list_info.dart
+++ lib/futures/current_todo_list_info/view/current_todo_list_info.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -96,6 +98,7 @@ class _CurrentTodoListInfoState extends State<CurrentTodoListInfoPage> {
                             child: Lottie.asset('assets/lottie/voice.json',
                                 fit: BoxFit.fill,
                                 errorBuilder: (context, error, stackTrace) {
+                              log('$error');
                               return Text('$error');
                             }),
                           ),
